594572be6ea9dfb61a933d7a63937dbcf7efd18a
jucasval <juanfrancastilla@gmail.com>
2021-12-25 13:18:02 +0100

    Estructura del proyecto

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -12,9 +13,17 @@ public class HelloWorld extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws IOException {
+		
+		Label label = new Label("Hola mundo");
 		Pane root = new Pane();
-		primaryStage.setTitle("Hello World");
-		primaryStage.setScene(new Scene(root, 300, 275));
+		
+		root.getChildren().add(label);
+		
+		
+		
+		Scene scene = new Scene(root, 300, 275);
+		
+		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 
