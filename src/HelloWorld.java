597e4ebc5e9b0c79f4d17bcdd63d7a217e0eb898
jucasval <juanfrancastilla@gmail.com>
2021-12-25 13:02:40 +0100

    Creación del proyecto

---
+++ src/HelloWorld.java
@@ -0,0 +1,24 @@
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class HelloWorld extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		Pane root = new Pane();
+		primaryStage.setTitle("Hello World");
+		primaryStage.setScene(new Scene(root, 300, 275));
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
