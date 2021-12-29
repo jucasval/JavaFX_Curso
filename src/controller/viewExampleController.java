4f7a3529efb30578af14d4485c318376e48640fa
jucasval <juanfrancastilla@gmail.com>
2021-12-29 12:54:02 +0100

    Manejo de buttons en javaFX

--- src/controller/viewExampleController.java
+++ src/controller/viewExampleController.java
@@ -3,6 +3,8 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javax.swing.JOptionPane;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,8 +13,14 @@ import javafx.scene.control.Label;
 
 public class viewExampleController implements Initializable{
 	
+	@FXML
+	private Button btnAceptar;
 	
-
+	@FXML
+	private void event_btnAceptar(ActionEvent event) {
+		JOptionPane.showMessageDialog(null, "Hola Mundo");
+	}
+	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		// TODO Auto-generated method stub
